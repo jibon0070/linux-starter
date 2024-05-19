c89f82c9b4c943eaaf070d67cb53a29e9b8296cc
A.R. Jibon <atikurrahaman386@gmail.com>
2024-05-20 01:58:20 +0600

    build: Update logic to check for npm version before installation
    Refactor the nodejs installation script to only install npm if the version is not
    already installed, improving efficiency.

--- scripts/install/nodejs.sh
+++ scripts/install/nodejs.sh
@@ -3,7 +3,11 @@
 if ! isInstalled npm; then
 	nala install npm
 else
-	echo "npm is already installed! skipping install."
+	if [ $(npm -v | grep -E "^[0-9]+\.[0-9]+\.[0-9]+$") -eq 0 ]; then
+		nala install npm
+	else
+		echo "npm is already installed! skipping install."
+	fi
 fi
 
 if ! isInstalled n "\sn@" 1 "npm list -g --depth=0"; then
