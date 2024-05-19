a3774ea9a2e540bed9ce08374c05c958869af285
A.R. Jibon <atikurrahaman386@gmail.com>
2024-05-20 01:35:33 +0600

    build: Update nodejs install script
    - Removed redundant check for nodejs installation
    - Added reboot and exit after installing npm or n lts
    - Improved error handling for npm i -g n command

--- scripts/install/nodejs.sh
+++ scripts/install/nodejs.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-if ! isInstalled nodejs; then
-	nala install nodejs
-else
-	echo "nodejs is already installed! skipping install."
-fi
 if ! isInstalled npm; then
 	nala install npm
 else
@@ -12,11 +7,17 @@ else
 fi
 
 if ! isInstalled n "\sn@" 1 "npm list -g --depth=0"; then
-	npm i -g n
+	if npm i -g n; then
+		echo "reboot"
+		exit 1
+	fi
 else
 	echo "n is already installed! skipping install."
 fi
 
-n lts
+if n lts; then
+	echo "reboot"
+	exit 1
+fi
 
 exit 0
