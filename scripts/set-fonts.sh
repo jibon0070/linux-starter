97c78754cc97db46f261ecaa16bf170d7f15cab6
A.R. Jibon <atikurrahaman386@gmail.com>
2024-05-20 17:28:32 +0600

    feat: Install Nerd Fonts CascadiaCode.zip for better terminal experience

--- scripts/set-fonts.sh
+++ scripts/set-fonts.sh
@@ -6,8 +6,19 @@ else
 	echo "Terminus fonts already installed! skipping..."
 fi
 
+nala install unzip -y --simple
+
 # Set the font to Terminus Fonts
 setfont /usr/share/consolefonts/Uni3-TerminusBold28x14.psf.gz
+
+if [ ! -d /home/$username/.fonts ]; then
+	mkdir /home/$username/.fonts
+fi
+
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/CascadiaCode.zip -O $builddir/font.zip
+unzip $builddir/font.zip -d /home/$username/.fonts
+chown $username:$username /home/$username/.fonts -R
+rm $builddir/font.zip
 
 # Reloading Font
 fc-cache -vf
