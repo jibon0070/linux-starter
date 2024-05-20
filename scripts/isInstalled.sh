5c6c36e0e5d1deea989b7184b976a48db7576be8
A.R. Jibon <atikurrahaman386@gmail.com>
2024-05-21 00:29:48 +0600

    refactor: Update isInstalled function to check package installation using 'command -v' instead of 'apt-cache search' for efficiency.

--- scripts/isInstalled.sh
+++ scripts/isInstalled.sh
@@ -1,26 +1,7 @@
 #!/bin/bash
 
 function isInstalled {
-	if ! [ -v 1 ]; then
-		echo "No package name given."
-		exit 1
-	fi
-	if [ -v 2 ]; then
-		pattern=$2
-	else
-		pattern="^$1\s"
-	fi
-	if [ -v 3 ]; then
-		count=$3
-	else
-		count=1
-	fi
-	if [ -v 4 ]; then
-		command=$4
-	else
-		command="apt-cache search $1"
-	fi
-	if ! [ $($command | grep -E $pattern | wc -l) -ge $count ]; then
+	if [ $(command -v $1 | wc -l) -eq 1 ]; then
 		return 1
 	fi
 	return 0
