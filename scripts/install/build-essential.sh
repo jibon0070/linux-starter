6387e758c0558a47b968ce0b3ad285abfcd197f6
A.R. Jibon <atikurrahaman386@gmail.com>
2024-05-21 13:41:32 +0600

    added curl install

--- scripts/install/build-essential.sh
+++ scripts/install/build-essential.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-if ! isInstalled build-essential; then
-	nala install build-essential nautilus gnome-disk-utility -y --simple
-else
-	echo "build-essential already installed"
-fi
+nala install build-essential nautilus gnome-disk-utility curl -y --simple
